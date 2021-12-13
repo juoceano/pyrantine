c94b47f200ce3d7d06e80c434c497e768e91bd39
Juliana Leonel <juoceano@gmail.com>
2021-12-13 13:49:21 -0300

    Delete notebooks/01-mapping directory

--- notebooks/01-mapping/01-basemap_cartopy-00-sol.py
+++
@@ -1,8 +0,0 @@
-ax0 = plt.axes();
-ax1 = plt.axes(projection=ccrs.PlateCarree());
-
-mpl = [obj for obj in dir(ax0) if not obj.startswith('_')]
-cart = [obj for obj in dir(ax1) if not obj.startswith('_')]
-
-print(set(mpl).symmetric_difference(cart))
-print(ax1.get_extent())
